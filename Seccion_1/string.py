61ec4a7de338eae2ad706767e7acc0d8d11c9bf3
Heran76 <heran76@gmail.com>
2024-10-29 08:19:15 +0100

    String

---
+++ Seccion_1/string.py
@@ -0,0 +1,11 @@
+#string - cadena de texto va con comillas para decir donde empieza y termina;
+#no son numeros son caracteres a ir en comillas
+print('100 + 50') 
+#concatenar string
+print("Holla "+" Caracola")
+#caracter especiales poner comillas en un texto 
+print("Me llamo \"Antonio\"")
+#nueva linea en un print \n
+print("Esta es la primera linea,\nesta es la segunda linea,\nesta es la tercera linea")
+#tabulaciones \t
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
