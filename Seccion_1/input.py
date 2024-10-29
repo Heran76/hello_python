8b731e1e3b7a19623b8a255a95b0d5c1a5e4f0c0
Heran76 <heran76@gmail.com>
2024-10-29 09:12:28 +0100

    Input

---
+++ Seccion_1/input.py
@@ -0,0 +1,7 @@
+#ingresar por consola informacion input
+
+print("Tu nombre es "+ input("Introduce tu nombre: "))
+#Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+#Escribe tu nombre:
+#Escribe tu apellido:
+print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido:"))
