ae99c3ca019d7dd3b38203f3f972a43fdc70821b
Heran76 <heran76@gmail.com>
2024-10-30 12:13:23 +0100

    tipos de datos

---
+++ Seccion_2/tipos_datos.py
@@ -0,0 +1,21 @@
+#tipos de datos : string(str), integer(int), floating(float), lista([list]), diccionanrio{dic 'calve':'valor'} tuple(orden no se modigica), set{'elementos unico'}, boleano(true, false) 
+
+#variables
+nombre = 'Antonio'; #variabel tipo string
+print(nombre);
+
+nombre = "Marcos";
+print(nombre);
+
+edad = 48; #variable tipo int
+print(edad); 
+
+apellido = input('¿cual es tu apellido? : ');
+print('Tu apellidos es '+apellido);
+
+#concatenacion de varialbes
+
+num1 = 40;
+num2 = 2;
+suma = num1+num2;
+print(f'la suma de los numeros es : {suma}');
