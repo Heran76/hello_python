bcfac09510fd918a2f5eb9bc04af79d8ae003ac3
Heran76 <heran76@gmail.com>
2024-10-30 12:14:07 +0100

    Integers float

---
+++ Seccion_2/integers_float.py
@@ -0,0 +1,10 @@
+#integers  - integers(int) numeros enteros, float(numeros decimales)
+
+mi_numero = 1;
+print(mi_numero);
+print(type(mi_numero)) #type nos dice que tipo de varaialbe es
+
+mi_numero2 = 4.2; #tipo float
+print(mi_numero2);
+print(type(mi_numero2));
+
