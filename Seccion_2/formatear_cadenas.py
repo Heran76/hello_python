6a8e82dcd1b9890a23e9fa033c6c456fe4096639
Heran76 <heran76@gmail.com>
2024-11-02 19:33:33 +0100

    Formatear

---
+++ Seccion_2/formatear_cadenas.py
@@ -0,0 +1,18 @@
+#formatear cadenas.
+"""
+1-Función format
+"""
+  #print("Mi coche es {} y de matricula{}".format(color_coche,matricula))
+print("*************")
+print(" ")
+num1 = 10
+num2 = 32
+print("mi num1 es {} y mi num2 {}".format(num1,num2));
+print("*************")
+print(" ")
+"""
+2-Función format
+"""
+ # print(f"Mi coche es {color_coche}y de matricula{matricula}")
+
+print(f"El numero1 {num1} y el numero num2 es {num2}") #con f para decir cada literal
