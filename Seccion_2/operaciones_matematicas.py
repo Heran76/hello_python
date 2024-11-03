5d31be1ec4f6212163d8229ffdfa012f7fbe8d46
Heran76 <heran76@gmail.com>
2024-11-03 17:46:20 +0100

    Operaciones matemáticas

---
+++ Seccion_2/operaciones_matematicas.py
@@ -0,0 +1,58 @@
+#operaciones matematicas
+x = 6;
+y = 2;
+z=  7;
+
+#suma
+print('---------------------')
+print('(** Suma **)')
+print(f'La suma de {x} + el valor de {y} es = a { x + y}');
+print('---------------------')
+print(' ')
+
+#resta
+print('---------------------')
+print('(** Resta **)')
+print(f'La resta de {x} y {y} es = a {x - y} ')
+print('---------------------')
+print(' ')
+
+#Multiplicar
+print('---------------------')
+print('** Multiplicar **')
+print(f'La multiplicación de {x} x { y } es = {x * y}')
+print('---------------------')
+print(' ')
+
+#división
+print('---------------------')
+print('** Dicisión **')
+print(f'La division entre {x} y {y} es = {x/y}')
+print('---------------------')
+print(' ')
+
+#division al pisos
+print('---------------------')
+print('** Division al piso **')
+print(f'La division al piso de {y} entre {z} es igual a {z//y}')
+print('---------------------')
+print(' ')
+
+#Modulo
+print('---------------------')
+print('** Módulo **')
+print(f'El modulo de {y} entre {x} es igaul a {x%y}')
+print('---------------------')
+print(' ')
+
+#Elevado
+print('---------------------')
+print('** Numero elvado **')
+print(f'EL numero {z} elevado a {x} es = {z**y}')
+print('---------------------')
+print(' ')
+
+#Raiz cuadrada
+print('---------------------')
+print('** La raiz cuadrad **')
+print(f'La raiz cuadra de {x} es igual {x**0.5}')
