ab13fe776ead502a94902abcac280fd36fe89b32
Heran76 <heran76@gmail.com>
2024-11-10 13:07:10 +0100

    Set

--- Seccion_3/set.py
+++ Seccion_3/set.py
@@ -70,3 +70,17 @@ print(mi_set3)
 
 sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
 print(sorteo.pop())
+
+#Práctica Sets 3
+#Agrega el nombre Damián al siguiente set, 
+# utilizando métodos de sets:
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+
+#Práctica Sets 3
+#Agrega el nombre Damián al siguiente set, 
+# utilizando métodos de sets:
+
+sorteo = {"Camila", "Margarita", "Axel", "Jorge", "Miguel", "Mónica"}
+sorteo.add("Damián")
+print(sorteo)
