f99842fd9a17fb146dd120bc1cebb65fa1c55b2f
Heran76 <heran76@gmail.com>
2024-11-12 13:20:56 +0100

    comparadores

--- seccion_4/operadores_comparacion.py
+++ seccion_4/operadores_comparacion.py
@@ -10,3 +10,20 @@ num1 = 36
 num2 = 17
 mi_bool = num1 >= num2
 print(mi_bool);
+
+'''
+Práctica Operadores de Comparación 2
+Crea dos variables (num1 y  num2):
+
+Dentro de num1, almacena el resultado de la operación raíz cuadrada de 25
+
+Dentro de num2, almacena el número 5.
+
+Verifica si num1 es igual a num2 y almacena el resultado de dicha comparación 
+en una variable llamada mi_bool.
+'''
+num3 = 25**0.5
+num4 = 5
+
+mi_bool2 = num3 == num4
+print(mi_bool2)
