b23a7062164d41d091bf8af2c50a321abe9be698
Heran76 <heran76@gmail.com>
2024-11-12 13:52:51 +0100

    Practica 2

--- seccion_4/operadores_logicos.py
+++ seccion_4/operadores_logicos.py
@@ -16,6 +16,26 @@ resultado de dicha comparación en una variable llamada mi_bool
 num1 = 36
 num2 = 72/2
 num3 = 48
- 
- 
 mi_bool = num1 > num2 and num1 < num3
+print(mi_bool)
+ 
+'''
+Práctica Operadores Lógicos 2
+Crea tres variables (num1 ,  num2 y num3):
+
+Dentro de num1, almacena el valor 36
+
+Dentro de num2, almacena el resultado de la operación 72/2
+
+Dentro de num3, almacena el valor 48
+
+Verifica si num1 es mayor que num2, o menor que num3. 
+Almacena el resultado de dicha comparación en una variable llamada mi_bool.
+
+'''
+num4 = 36
+num5 = 72/2
+num6 = 48
+mi_bool2 = num1 > num2 or num1 < num3
+print(mi_bool2)
+
