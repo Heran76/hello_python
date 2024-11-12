60778f4aacf10b778770803b3339455e1275a38a
Heran76 <heran76@gmail.com>
2024-11-12 13:47:42 +0100

    Pratica 1

---
+++ seccion_4/operadores_logicos.py
@@ -0,0 +1,21 @@
+#Operadores lógicos. y(and)  o(or) no(not)
+'''
+Práctica Operadores Lógicos 1
+Crea tres variables (num1 ,  num2 y num3):
+
+Dentro de num1, almacena el valor 36
+
+Dentro de num2, almacena el resultado de la operación 72/2
+
+Dentro de num3, almacena el valor 48
+
+Verifica si num1 es mayor que num2, y menor que num3. Almacena el 
+resultado de dicha comparación en una variable llamada mi_bool
+
+'''
+num1 = 36
+num2 = 72/2
+num3 = 48
+ 
+ 
+mi_bool = num1 > num2 and num1 < num3
