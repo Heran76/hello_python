90afbb5c5584fdf40b02ec5280d5c4b66c120621
Heran76 <heran76@gmail.com>
2024-11-15 06:50:27 +0100

    Operadores

---
+++ mouredev_pro/operadores/operadores.py
@@ -0,0 +1,53 @@
+# 1. Realiza las siguientes operaciones aritméticas:
+# Suma: 15 + 25
+# Resta: 50 - 22
+# MultiplicaciÃ³n: 8 * 7
+# DivisiÃ³n: 100 / 20
+
+print(15 + 25)
+print(50 - 22)
+print(8 * 7)
+print(100 / 20)
+
+# 2. Calcula el resto de la división de 37 entre 5 y almacénalo en una variable remainder. Luego imprí­melo.
+
+remainder = 37 % 5
+print(remainder)
+
+# 3. Convierte el número 7 en una cadena de texto y concaténalo con la frase  es mi número favorito. Imprime el resultado.
+
+number = 7
+result = str(number) + " es mi número favorito"
+print(number)
+
+# 4. Repite la palabra â€œPythonâ€ 10 veces usando el operador de multiplicaciÃ³n para cadenas y luego imprÃ­mela.
+
+print("Python" * 10)
+
+# 5. Crea dos variables: a y b con los valores 12 y 8 respectivamente. Compara si a es mayor que b y almacena el resultado en una variable booleana resultado. Imprime el valor de resultado.
+
+a = 12
+b = 8
+result = a > b
+print(result)
+
+# 6. Compara dos cadenas de texto (â€œappleâ€ y â€œbananaâ€) usando los operadores > y < y explica cuÃ¡l tiene mayor orden alfabÃ©tico.
+
+print("apple" > "banana")
+print("apple" < "banana")
+
+# 7. Realiza una comparaciÃ³n lÃ³gica usando and para verificar si el nÃºmero 10 es mayor que 5 y menor que 20. Imprime el resultado.
+
+print(10 > 5 and 10 < 20)
+
+# 8. Usa el operador or para verificar si el nÃºmero 7 es menor que 3 o mayor que 5. Imprime el resultado.
+
+print(7 < 3 or 7 > 5)
+
+# 9. Aplica el operador not para invertir el resultado de la comparaciÃ³n 15 > 20. Â¿CuÃ¡l es el resultado?
+
+print(not (15 > 20))
+
+# 10. Combina operadores aritmÃ©ticos y lÃ³gicos: Verifica si el nÃºmero resultante de la expresiÃ³n (5 * 3) + 2 es mayor que 10 y menor que 20. Imprime el resultado.
+
+print((5 * 3) + 2 > 10 and (5 * 3) + 2 < 20)
