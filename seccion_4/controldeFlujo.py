eb137049bef07816182b2e1b1a5f9fba33c86305
Heran76 <heran76@gmail.com>
2024-11-23 13:32:38 +0100

    Crontol de Flujo

--- seccion_4/controldeFlujo.py
+++ seccion_4/controldeFlujo.py
@@ -120,3 +120,39 @@ elif edad < 18:
     print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
 else:
     print("No puedes conducir. Necesitas contar con una licencia")
+
+'''
+Práctica Control de Flujo 3
+Para acceder a un determinado puesto de trabajo, el candidato debe ser capaz de programar en Python y tener conocimientos de inglés.
+
+Crea una estructura condicional para evaluar a un candidato dadas estas condiciones, y muestra el mensaje correspondiente en pantalla:
+
+"Cumples con los requisitos para postularte"
+
+"Para postularte, necesitas saber programar en Python y tener conocimientos de inglés"
+
+"Para postularte, necesitas tener conocimientos de inglés"
+
+"Para postularte, necesitas saber programar en Python"
+
+Utiliza la base de código ya proporcionada para plantear la estructura de control de flujo apropiada y verificar dichas condiciones. Evalúa a un candidato que sabe inglés, pero no programa en Python.
+
+'''    
+habla_ingles = True
+sabe_python = False
+
+"Cumples con los requisitos para postularte"
+
+"Para postularte, necesitas saber programar en Python y tener conocimientos de inglés"
+
+"Para postularte, necesitas tener conocimientos de inglés"
+
+"Para postularte, necesitas saber programar en Python"
+if habla_ingles and sabe_python:
+    print("Cumples con los requisitos para postularte")
+elif (not habla_ingles) and (not sabe_python):
+    print("Para postularte, necesitas saber programar en Python y tener conocimientos de inglés")
+elif not habla_ingles:
+    print("Para postularte, necesitas tener conocimientos de inglés")
+else:
+    print("Para postularte, necesitas saber programar en Python")
