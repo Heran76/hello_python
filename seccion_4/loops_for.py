21166ce8c7443430255688da5b45830a6d664622
Heran76 <heran76@gmail.com>
2024-11-27 18:49:42 +0100

    recorrer con indice

--- seccion_4/loops_for.py
+++ seccion_4/loops_for.py
@@ -4,3 +4,18 @@ nombres = ['Antonio','Marcos','Luis','Belen','Jose']
 
 for i  in nombres:
     print("Hola :" + i)
+lista = ['a','b','c']
+print(" ")
+print("***********")
+print(" ")
+for letra in lista:
+    print("letra : " + letra)
+print(" ")
+print("***********")
+print(" ")
+print("Recorer con indice")
+lista1 = ['a','b','c','d','e']
+for i in lista1:
+    numero_letra = lista1.index(i) + 1
+    print(f"letra {numero_letra} : {i}")
+    
