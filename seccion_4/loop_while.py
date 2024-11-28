bc8a38c648db94cd40626bf822300f06336df8c7
Heran76 <heran76@gmail.com>
2024-11-28 16:58:04 +0100

    Practica 1

--- seccion_4/loop_while.py
+++ seccion_4/loop_while.py
@@ -4,4 +4,13 @@ while monedas > 0:
  print(f"Tengo {monedas} monedas")
  monedas -= 1
  
- 
+'''
+Práctica Loop While 1
+Crea un Loop While que se imprima en pantalla los números 
+del 10 al 0, uno a la vez.
+'''
+
+numero = 10
+while numero >=0:
+    print(numero)
+    numero -= 1
