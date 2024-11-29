6d5e213845c196a49c4bed22692bf0011ee82ae5
Heran76 <heran76@gmail.com>
2024-11-29 19:41:46 +0100

    ejercicio 1

---
+++ mouredev_pro/listas/listas.py
@@ -0,0 +1,12 @@
+### List ###
+
+my_list = list() #la inicializamos como un variable
+my_other_list = []
+
+print(len(my_list)) #longitud de lista nos dice los elementos
+
+my_list = []
+#### Ejercicios ####
+# 1. Crea una lista con los números del 1 al 5 e imprímela.
+my_list = [1,2,3,4,5]
+print(my_list)
