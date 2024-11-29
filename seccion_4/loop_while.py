ff3826204cbfa849cdbfbc06a7aaea284319533b
Heran76 <heran76@gmail.com>
2024-11-29 18:43:02 +0100

    Lopp_While

--- seccion_4/loop_while.py
+++ seccion_4/loop_while.py
@@ -40,6 +40,7 @@ lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
 No debes cambiar el orden de la lista.
 
 '''
+
 lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
  
 for numero in lista_numeros:
