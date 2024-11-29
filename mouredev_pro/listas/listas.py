521fe27bc90f3ac6fb907b61ed76c9230d8e866d
Heran76 <heran76@gmail.com>
2024-11-29 20:28:53 +0100

    ejercicio 3

--- mouredev_pro/listas/listas.py
+++ mouredev_pro/listas/listas.py
@@ -10,3 +10,17 @@ my_list = []
 # 1. Crea una lista con los números del 1 al 5 e imprímela.
 my_list = [1,2,3,4,5]
 print(my_list)
+
+#2. Accede e imprime el tercer elemento de la lista [10,20,30,40,50]
+my_list1 = [10,20,30,40,50]
+print(my_list1[2])
+
+#3. Agrega el numero 6 al final de lista [1,2,3,4,5]
+my_list2 =[1,2,3,5]
+my_list2.append(6)
+print(my_list2)
+
+#4 Inserta el número 15 en la posición n2 de lista [10,20,30,40,50]
+my_list3 = [10,20,30,40,50]
+my_list3.insert(2,15)
+print(my_list3)
