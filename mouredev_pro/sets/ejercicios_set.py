ad4a011d1205be97b91bee1eeba3b627871beb93
Heran76 <heran76@gmail.com>
2024-12-03 20:09:47 +0100

    ejercicio1

---
+++ mouredev_pro/sets/ejercicios_set.py
@@ -0,0 +1,22 @@
+# 1. Crea un set con los números del 1 al 5 e imprí­melo.
+
+my_set = {1,2,3,4,5}
+print(my_set)
+
+# 2. Añade el número 6 al set {1, 2, 3, 4, 5} e imprí­melo.
+
+# 3. Intenta aÃ±adir el nÃºmero 5 al set {1, 2, 3, 4, 5} nuevamente. Â¿QuÃ© sucede?
+
+# 4. Verifica si el nÃºmero 3 estÃ¡ en el set {1, 2, 3, 4, 5} e imprime el resultado.
+
+# 5. Elimina el nÃºmero 4 del set {1, 2, 3, 4, 5} e imprime el set resultante.
+
+# 6. Usa el mÃ©todo clear() para vaciar un set y luego imprime su longitud.
+
+# 7. Convierte el set {"manzana", "naranja", "plÃ¡tano"} en una lista e imprime el primer elemento de la lista.
+
+# 8. Realiza la uniÃ³n de dos sets: {1, 2, 3} y {4, 5, 6}, e imprime el set resultante.
+
+# 9. Calcula la diferencia entre los sets {1, 2, 3, 4} y {3, 4, 5, 6} e imprime el resultado.
+
+# 10. Elimina un set llamado my_set usando del y luego intenta imprimirlo para ver el resultado
