2fa0aa38feb936878d521f61e2b2ed220ef85fc1
Heran76 <heran76@gmail.com>
2024-12-06 21:19:10 +0100

    dicionarios

---
+++ mouredev_pro/diccionarios/dicionarios.py
@@ -0,0 +1,7 @@
+#Dicionarios
+'''
+Los diccionarios son colecciones que almacenan pares de clave-valor,
+permitiendo acceder a los valores de manera rápida mediante su clave. Esto hace que los diccionarios
+sean perfectos para representar datos estructurados, como las entradas de una base de datos o la 
+configuración de una aplicación.
+'''
