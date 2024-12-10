2a7043091fcc7d78445fd916bcf0e3ebd454f16b
Heran76 <heran76@gmail.com>
2024-12-10 20:48:34 +0100

    Condicionales

--- mouredev_pro/condicionales/conditionals.py
+++ mouredev_pro/condicionales/conditionals.py
@@ -9,12 +9,24 @@ En esta lección, veremos cómo Python evalúa expresiones y ejecuta ciertos blo
 código en funciones de esasa condiciones.
 '''
 #if
-my_condition = False
+my_condition = True
 if my_condition: # es lo mismo que if my_condition == True:
     print("Se ejecuta la condición if")
+ 
 
-my_condition = 5 * 5
+my_condition = 5 * 2
 if my_condition == 10:
     print("Se ejecuta la segunda ejecuión del seguno if")
+#if, elif, else
+
+if my_condition > 10 and my_condition < 20 :
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 25:
+    print("Es igual a 25")
+else:
+    print("Es mayor o igual que 10 o mayor o igual que 20 o distinto de 25")
+
+print("La ejecución continua")                
+    
     
     
