c54e0f8a4a6e171dfd6e57fa746830e75fd79bfc
Heran76 <heran76@gmail.com>
2024-12-10 20:24:18 +0100

    condidicionales

--- mouredev_pro/condicionales/conditionals.py
+++ mouredev_pro/condicionales/conditionals.py
@@ -12,3 +12,9 @@ código en funciones de esasa condiciones.
 my_condition = False
 if my_condition: # es lo mismo que if my_condition == True:
     print("Se ejecuta la condición if")
+
+my_condition = 5 * 5
+if my_condition == 10:
+    print("Se ejecuta la segunda ejecuión del seguno if")
+    
+    
