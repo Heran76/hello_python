b01347e59a1374ad0043a84a48ce910f920985ff
Heran76 <heran76@gmail.com>
2024-12-10 20:14:48 +0100

    Condicionales

---
+++ mouredev_pro/condicionales/conditionals.py
@@ -0,0 +1,10 @@
+### Conditionals ####
+'''
+Los condicionales son una de las extructuras en cualquier lenguaje de progrmacion
+ya que permiten tomar decisiones basadas en condiciones lógicas. En Python. utilizamos
+principalmente la sentencia if, elif y else para controlar el flujo de ejecución
+dependiendo de si una condición se cumple o no.
+
+En esta lección, veremos cómo Python evalúa expresiones y ejecuta ciertos bloques de
+código en funciones de esasa condiciones.
+'''
