c04df00bbbdeb2fe8cffa1220ac2936bc2f3bd4b
Heran76 <heran76@gmail.com>
2024-12-10 21:14:49 +0100

    Ejercicio 1

---
+++ mouredev_pro/condicionales/ejercicios.py
@@ -0,0 +1,10 @@
+# Ejercicios Condicionales
+# 1. Escribe un programa que verifique si un número es positivo, negativo o cero.
+print(" ")
+number = int(input("Introduce un número : "))
+if number > 0:
+    print("El número es positivo")
+elif number < 0:
+    print("El número es negativo") 
+else:
+    print("El número es igual a 0")       
