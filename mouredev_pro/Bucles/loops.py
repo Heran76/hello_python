2969d0d64620d11e38ab89e69824ba2914bb9abd
Heran76 <heran76@gmail.com>
2024-12-12 17:23:05 +0100

    bucles

---
+++ mouredev_pro/Bucles/loops.py
@@ -0,0 +1,17 @@
+## Bucles - loops
+'''
+Los bucles nos permiten repetir bloques de código de forma eficiente
+evitnado la necesidad de escribir código una y otra vez. En Python. los
+bucles son esenciales para trabajar con colecciones de datos y ejecutar tareas
+repetitivas de manera rápida y limpia, En esta lección, aprenderemos sobre los dos
+tipos principales de bucles en Python: while y for
+
+1.- El bucle while: Este tipo de bucle ejecuta un bloque de código mientras una
+condicion sea verdadera. Es ideal cuando no sabemos exactamente cuántas veces se
+debe repetir el ciclo, pero tenemos una condición que debe cumplirse para continuar
+la ejecución.
+
+2.- El bucle for: Este tipo de bucle se utiliza principalmente para iterar sobre colecciones
+de datos, como listas, tuplas, sets y diccionarios, Es perfecto para recorrer cada elemento de una
+colección de forma ordenada.
+'''
