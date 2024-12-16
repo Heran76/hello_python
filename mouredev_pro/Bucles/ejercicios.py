68865e28c8111f7cecbb9909cdffaceca527ec9f
Heran76 <heran76@gmail.com>
2024-12-16 18:23:10 +0100

    ejercicios 2

--- mouredev_pro/Bucles/ejercicios.py
+++ mouredev_pro/Bucles/ejercicios.py
@@ -1,4 +1,4 @@
-#ejercicios bucles while
+#ejercicios bucles 
 # 1. Usa un bucle while para imprimir los números del 1 al 10.
 print(" ")
 print("***** Ejercicio 1 *****")
@@ -6,3 +6,10 @@ number = 1
 while number <= 10:
     print(number)
     number += 1
+    # 2. Usa un bucle for para recorrer la lista[10, 20, 30, 40, 50] e imprime cada número.
+print(" ")
+print("***** Ejercicio 2 *****")
+my_list = [10,20,30,40,50]
+for i in my_list:
+        print(i)
+    
