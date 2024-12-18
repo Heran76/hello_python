2a59067ee198bee229db81f951fac78422425669
Heran76 <heran76@gmail.com>
2024-12-18 20:16:37 +0100

    funciones

---
+++ mouredev_pro/funciones/funciones.py
@@ -0,0 +1,11 @@
+#funciones.
+'''
+las funciones son bloques de código reutilizables que nos
+permiten organizar nuestro código de manera eficiente, reducir la
+repetición y hacerlo mas legible y fácil de mantener.
+
+Las funciones nos permiten dividir grandes programas en pequeñas piezas
+manejables facilitando la compresión y el matenimiento de nuestro código
+Además nos permiten ejecutar una sentencia de intruciones simplemente llamándolas
+por su nombre.
+'''
