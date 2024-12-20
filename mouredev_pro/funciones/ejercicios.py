f95f57c1980e9166d716797e778b728a57dcc38a
Heran76 <heran76@gmail.com>
2024-12-20 18:33:00 +0100

    Ejercicio-Funciones-3

--- mouredev_pro/funciones/ejercicios.py
+++ mouredev_pro/funciones/ejercicios.py
@@ -1,6 +1,20 @@
 #Ejercicios funciones#
+def separador():
+    print(" ")
+    print("*******")
+
 # 1. Crea una función llamada "personalized_greeting" que reciba un nombre como argumento e imprima "Hola, <nombre>". Si no se proporciona ningÃºn nombre, debe saludar diciendo "Hola, desconocido".
 def personalized_greeting(name="desconocido"):
     print(f"Hola, {name}")
 personalized_greeting("Antonio")    
+# 2. Escribe una función llamada "multiply" que reciba dos números como argumentos y retorne el resultado de multiplicarlos.
+separador()
+def multiply(first_number, second_number):
+    return first_number * second_number
+print(multiply(2, 3))
+# 3. Crea una función llamada "is_even" que reciba un número entero como argumento y retorne True si es par y False si es impar.
+def is_even(number):
+    return number % 2 == 0
+separador()
+print(is_even(4))
 
