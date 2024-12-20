52cf29423bad14546d9d14fe5003efa20530016a
Heran76 <heran76@gmail.com>
2024-12-20 18:22:35 +0100

    Ejercicio1-funciones

---
+++ mouredev_pro/funciones/ejercicios.py
@@ -0,0 +1,6 @@
+#Ejercicios funciones#
+# 1. Crea una función llamada "personalized_greeting" que reciba un nombre como argumento e imprima "Hola, <nombre>". Si no se proporciona ningÃºn nombre, debe saludar diciendo "Hola, desconocido".
+def personalized_greeting(name="desconocido"):
+    print(f"Hola, {name}")
+personalized_greeting("Antonio")    
+
