7ab7f1c490fcab6022cf370b0b6f82d36764cee9
Heran76 <heran76@gmail.com>
2024-12-26 20:03:13 +0100

    Clases

--- mouredev_pro/clases/clases.py
+++ mouredev_pro/clases/clases.py
@@ -5,6 +5,45 @@ nuestro codigo de manera mas ordenada y mdoular
 Una clase en Python es como un plano o molde para crear objetos. Define
 las caracteristicas(atributos) y comportamientos(metodos) de los objetos derivados
 de esa clase tendrán. Esto nos permiten trabajar de manera más eficiente con datos complejos
-origanzando nuestro codigo en unidades mas manejables.
+origanzando nuestro codigo en unidades mas manejables. 
 
 '''
+### Classes ###
+
+# DefiniciÃ³n
+
+class MyEmptyPerson:
+    pass  # Para poder dejar la clase vacía
+
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+# Clase con constructor, funciones y propiedades privadas y públicas
+
+
+class Person:
+    def __init__(self, name, surname, alias="Sin alias"):
+        self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública
+        self.__name = name  # Propiedad privada
+
+    def get_name(self):
+        return self.__name
+
+    def walk(self):
+        print(f"{self.full_name} Está caminando")
+
+
+my_person = Person("Antonio", "Heredia")
+print(my_person.full_name)
+print(my_person.get_name())
+my_person.walk()
+
+my_other_person = Person("Antonio", "Heredia", "Heran76")
+print(my_other_person.full_name)
+my_other_person.walk()
+my_other_person.full_name = "ANtonio Heredia Morante(La práctica hace al maestro)"
+print(my_other_person.full_name)
+
+my_other_person.full_name = 666
+print(my_other_person.full_name)
