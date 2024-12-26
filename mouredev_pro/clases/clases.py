3e0d231a92742b225af0085a3e1984aba0132fa2
Heran76 <heran76@gmail.com>
2024-12-26 19:55:46 +0100

    clases

--- mouredev_pro/clases/clases.py
+++ mouredev_pro/clases/clases.py
@@ -1,1 +1,10 @@
 #clases
+'''
+las clases son la base de la creación de objetos y nos perminten estructurar
+nuestro codigo de manera mas ordenada y mdoular
+Una clase en Python es como un plano o molde para crear objetos. Define
+las caracteristicas(atributos) y comportamientos(metodos) de los objetos derivados
+de esa clase tendrán. Esto nos permiten trabajar de manera más eficiente con datos complejos
+origanzando nuestro codigo en unidades mas manejables.
+
+'''
