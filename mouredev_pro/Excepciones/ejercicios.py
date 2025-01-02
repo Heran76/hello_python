822ca362c43217a501b0e334bafe46a81f88054d
Heran76 <heran76@gmail.com>
2025-01-02 18:31:37 +0100

    Ejercicios_1

---
+++ mouredev_pro/Excepciones/ejercicios.py
@@ -0,0 +1,9 @@
+#Ejercicios Excepciones##
+# 1. Crea una función que intente dividir dos números proporcionados por el usuario. Usa try-except para capturar cualquier error de división (por ejemplo, división por cero).
+
+def divide_numbers(num1, num2):
+    try:
+        result = num1 / num2
+        print(f"El resultado es {result}")
+    except ZeroDivisionError:
+        print("Error: No se puede dividir entre cero.")
