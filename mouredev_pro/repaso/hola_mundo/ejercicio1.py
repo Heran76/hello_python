99df2847529cafdb571e94b1668df7d07fd175cc
Heran76 <heran76@gmail.com>
2025-06-02 21:49:54 +0200

    repasolunes

---
+++ mouredev_pro/repaso/hola_mundo/ejercicio1.py
@@ -0,0 +1,15 @@
+#hola mundo
+#1imprime un hola mundo por consola
+print("Hello World")
+#2escribe un comenteario de una sola linea explicando que hace el código
+
+#imprime tu nombre y edad en la misma línea utilizando la función print
+print("Mi nombre es Antonio y tengo", 37, "años")
+
+'''Usa la función type() para imprimir el tipo de dato de 
+una cadena de texto, un número entero y un número 
+decimal.'''
+
+print(type("Antonio"))
+print(type(48))
+print(type(48.5))
