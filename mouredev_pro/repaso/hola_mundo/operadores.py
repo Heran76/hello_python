dfe63656053c910cdeef23092774324f17435def
Heran76 <heran76@gmail.com>
2025-06-17 16:56:25 +0200

    Operadores_1

---
+++ mouredev_pro/repaso/hola_mundo/operadores.py
@@ -0,0 +1,16 @@
+'''
+Realiza las siguientes operaciones aritméticas: 
+Suma: 15 + 25 
+Resta: 50 - 22 
+Multiplicación: 8 * 7 
+División: 100 / 20 
+'''
+suma = 15 + 25
+resta = 50 - 22
+multiplicacion = 8*7
+division = 100 / 20
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
