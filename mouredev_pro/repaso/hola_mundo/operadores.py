9dfa9ce5a7b938b9bc4f5166f1663cf649b468c3
Heran76 <heran76@gmail.com>
2025-06-18 19:54:36 +0200

    OperadoresEj2

--- mouredev_pro/repaso/hola_mundo/operadores.py
+++ mouredev_pro/repaso/hola_mundo/operadores.py
@@ -9,8 +9,16 @@ suma = 15 + 25
 resta = 50 - 22
 multiplicacion = 8*7
 division = 100 / 20
-
+print("*****ejecicio1*****")
 print(suma)
 print(resta)
 print(multiplicacion)
 print(division)
+
+'''
+2. Calcula el resto de la división de 37 entre 5 y 
+almacénalo en una variable remainder. Luego imprímelo.
+'''
+print("*****ejecicio2*****")
+remainder = 37%5
+print(remainder)
